e180df8d7f610b63cb14058a4b84a410385c15ac
Michael Sedov <sedovmikh@yandex.ru>
2018-07-05 21:10:48 +0300

    first commit

---
+++ src/Application.java
@@ -0,0 +1,15 @@
+/**
+ * The {@code Application} class running
+ * bulls and cows application
+ *
+ * @author Michael "Mishima" Sedov
+ * @since 1.0.0
+ */
+
+public class Application
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello there! Welcome to bulls and cows console game! >__<");
+    }
+}
